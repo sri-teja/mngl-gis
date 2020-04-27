228023557acacd940a9ccfa989049d3b6124d261
sri-teja <asriteja77712@gmail.com>
2020-04-27 23:58:09 +0530

    Updates

---
+++ precache-manifest.689286dfa2735388b846f06742ad489b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0366c60afe867551e9bd851bb9b1307",
+    "url": "/index.html"
+  },
+  {
+    "revision": "36d83af33e0adab20565",
+    "url": "/static/css/main.6c614c2e.chunk.css"
+  },
+  {
+    "revision": "25974923e28f8aa36d4b",
+    "url": "/static/js/2.eaf8ba70.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.eaf8ba70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36d83af33e0adab20565",
+    "url": "/static/js/main.8c6de512.chunk.js"
+  },
+  {
+    "revision": "e403274ee826bd343bea",
+    "url": "/static/js/runtime-main.b2fa27a4.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/mngl-gis/precache-manifest.8f0026d67439ea0d71eb58f2d3648b18.js"
+  "/precache-manifest.689286dfa2735388b846f06742ad489b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mngl-gis/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
