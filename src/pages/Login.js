a5b55b43847fc5d818d77d82aafd2f8fe5063f41
sri-teja <asriteja77712@gmail.com>
2020-04-27 19:58:42 +0530

    login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,36 +3,36 @@ import Layout from '../components/Layout'
 
 export default function Login(){
 
-  return (
-      <Layout>
-      <div className="all-wrapper menu-side with-pattern">
-            <div className="row">
-              <div className="col-6">  
-              </div>
-            <div className="col-6">  
-            <div className="auth-box-w">
-              <div className="logo-w pb-4">
-                <a href="/map"><img alt="" src="./mngl.png" width="50%"/></a>
-              </div>
-              <h4 className="auth-header">
-                Enterprise Integrity Asset Management System at MNGL
-              </h4>
-              <form action="">
-                <div className="form-group">
-                  <label for="">Username</label><input className="form-control" placeholder="Enter your username" type="text"/>
-                  <div className="pre-icon os-icon os-icon-user-male-circle"></div>
-                </div>
-                <div className="form-group">
-                  <label for="">Password</label><input className="form-control" placeholder="Enter your password" type="password"/>
-                  <div className="pre-icon os-icon os-icon-fingerprint"></div>
-                </div>
-                <div className="buttons-w">
-                    <a className="btn btn-primary" href="/map">Log in</a>
-                </div>
-              </form>
-              </div>
-              </div>
-            </div>
-          </div>
-          </Layout>
-      )}
+	return (
+			<Layout>
+			<div className="all-wrapper menu-side with-pattern">
+						<div className="row">
+							<div className="col-6">  
+							</div>
+						<div className="col-6">  
+						<div className="auth-box-w">
+							<div className="logo-w pb-4">
+								<a href="/map"><img alt="" src="./mngl.png" width="50%"/></a>
+							</div>
+							<h4 className="auth-header">
+								Enterprise Integrity Asset Management System at MNGL
+							</h4>
+							<form action="">
+								<div className="form-group">
+									<label for="">Username</label><input className="form-control" placeholder="Enter your username" type="text"/>
+									<div className="pre-icon os-icon os-icon-user-male-circle"></div>
+								</div>
+								<div className="form-group">
+									<label for="">Password</label><input className="form-control" placeholder="Enter your password" type="password"/>
+									<div className="pre-icon os-icon os-icon-fingerprint"></div>
+								</div>
+								<div className="buttons-w">
+										<a className="btn btn-primary" href="/map">Log in</a>
+								</div>
+							</form>
+							</div>
+							</div>
+						</div>
+					</div>
+					</Layout>
+			)}
