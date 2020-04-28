4ddde4f1782e9f9907dcbfcc80810fe35c6b11e9
sri-teja <asriteja77712@gmail.com>
2020-04-28 18:48:44 +0530

    Updates

---
+++ precache-manifest.157e6134ca0a462a79264882502a6e52.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9a6d5985a2d774a3ee6f17215bf8f05",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c2c6b4259fa968c28f00",
+    "url": "/static/css/main.6c614c2e.chunk.css"
+  },
+  {
+    "revision": "ec474f8748fd6d4df402",
+    "url": "/static/js/2.4f79f2c2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.4f79f2c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2c6b4259fa968c28f00",
+    "url": "/static/js/main.e464c86e.chunk.js"
+  },
+  {
+    "revision": "e403274ee826bd343bea",
+    "url": "/static/js/runtime-main.b2fa27a4.js"
+  }
+]);
